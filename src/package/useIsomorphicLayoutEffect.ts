eed3f02c7663a067ebc94a578826634a62e59315
RaulPeSilva <adraulpesilva@gmail.com>
2021-03-22 23:11:46 -0300

    chore: add isomorphicLayoutEffect

---
+++ src/package/useIsomorphicLayoutEffect.ts
@@ -0,0 +1,8 @@
+import { useLayoutEffect, useEffect } from 'react'
+
+export const useIsomorphicLayoutEffect =
+  typeof window !== 'undefined' &&
+  typeof window.document !== 'undefined' &&
+  typeof window.document.createElement !== 'undefined'
+    ? useLayoutEffect
+    : useEffect
