ea85a6aff6d1f9cb149bc67fcf0d78b9f50d885f
raulpesilva <adraulpesilva@gmail.com>
2021-06-22 01:22:14 -0300

    hotfix: export type

--- src/package/index.ts
+++ src/package/index.ts
@@ -3,7 +3,6 @@ import { unstable_batchedUpdates as batch } from './reactBatchedUpdates'
 export { createReState } from './createReState'
 export { createReStateDispatch } from './createReStateDispatch'
 export { createReStateSelect } from './createReStateSelect'
-export type { Selector, UniqueKey } from './types'
 export { useReState } from './useReState'
 export { useReStateSelector } from './useReStateSelector'
 setBatch(batch)
